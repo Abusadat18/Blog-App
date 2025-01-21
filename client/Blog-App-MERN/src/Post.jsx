34a7f8a45adf04263b3a997e95462201acd79fea
Abu Sadat Ansari <itssadatansari@gmail.com>
2025-01-21 10:57:15 +0530

    Build homepage with React

---
+++ client/Blog-App-MERN/src/Post.jsx
@@ -0,0 +1,23 @@
+function Post() {
+  return (
+    <>
+      {/* Post Start */}
+      <div className="post">
+        <div className="image">
+          <img src="https://www.catster.com/wp-content/uploads/2023/11/Beluga-Cat-e1714190563227.webp" alt=""/>
+        </div>
+        <div className="texts">
+          <h2>Hello World</h2>
+          <p className="info">
+            <a href="" className="author">Abu Sadat Ansari</a>
+            <time>2024-01-06 19:25</time>
+          </p>
+          <p className="summary">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque illo obcaecati id nisi, voluptatum, eaque eos esse ipsam distinctio consequatur ex, a enim dolores? Magnam pariatur accusantium necessitatibus eius? Sunt esse alias nulla earum eveniet pariatur nam delectus consectetur modi natus, non blanditiis beatae assumenda in fuga praesentium at nihil iure, repellat officiis sed optio? Vero numquam provident corporis consequuntur molestias culpa eum harum voluptatibus at quam, enim debitis quidem ex rem facilis quaerat? Alias ipsum architecto officia atque fuga optio. Voluptas cumque ratione aperiam fugit, aut nihil harum atque exercitationem repellat nesciunt necessitatibus cupiditate maiores cum vitae, unde odit?</p>
+        </div>
+      </div>
+      {/* Post Ends */}
+    </>
+  )
+}
+
+export default Post
